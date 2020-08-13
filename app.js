9787d418bbb5f09f0e9d7a31f1617faaf83bf828
Stean Kotze <steankotze@gmail.com>
2020-08-13 11:10:56 +0200

    [Backend] Imported data

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+const advance_voucher_presets = require('./data/advance_voucher_presets.json')
+const card_types = require('./data/card_types.json')
+const cards = require('./data/cards.json')
+const once_off_voucher_presets = require('./data/once_off_voucher_presets.json')
+const retailer_types = require('./data/retailer_types.json')
+const retailers = require('./data/retailers.json')
+const voucher_states = require('./data/voucher_states.json')
+const vouchers = require('./data/vouchers.json')
+
 App({
+  card_types: null,
+  cards: null,
+  retailer_types: null,
+  retailers: null,
+  voucher_presets: {
+    advance: null,
+    once_off: null
+  },
+  voucher_states: null,
+  vouchers: null,
   onLaunch(options) {
-    // Page opens for the first time
-    console.info('App onLaunch');
+    this.card_types = card_types
+    this.cards = cards
+    this.retailer_types = retailer_types
+    this.retailers = retailers
+    this.voucher_presets.advance = advance_voucher_presets
+    this.voucher_presets.once_off = once_off_voucher_presets
+    this.voucher_states = voucher_states
+    this.vouchers = vouchers
   },
   onShow(options) {
-    // Reopened by scheme from the background
-  },
-});
+    //
+  }
+})
