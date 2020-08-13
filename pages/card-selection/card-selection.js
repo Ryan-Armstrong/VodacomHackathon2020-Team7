8696a19dc3b51518cf5d7dd914182c4b14cf0729
Stean Kotze <steankotze@gmail.com>
2020-08-13 17:30:33 +0200

    [Frontend] Card Selection flow completed.

--- pages/card-selection/card-selection.js
+++ pages/card-selection/card-selection.js
@@ -2,14 +2,26 @@ const app = getApp()
 
 Page({
   data: {
-    cards: null
+    cards: null,
+    card: null
   },
   onLoad() {
     this.setData({
       cards: app.get('/cards')
     })
   },
-  next(){
-    my.navigateTo({ url: '../payment-summary/payment-summary' });
+  setCard(event) {
+    this.setData({
+      card: this.data.cards[event.target.dataset.id]
+    })
+  },
+  gotoPaymentSummary() {
+    if (this.data.card != undefined)
+    {
+      app.paymentInformation['card'] = this.data.card
+      my.navigateTo({
+        url: '../payment-summary/payment-summary'
+      })
+    }
   }
-});
+})
