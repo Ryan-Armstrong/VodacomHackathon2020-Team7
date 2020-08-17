e8249db73cdee7b0971019060fc1fd2a5d92065c
Stean Kotze <steankotze@gmail.com>
2020-08-17 10:09:53 +0200

    [Frontend] Updated Cards to have titles

--- .tea/entryFiles-development/config$.js
+++ .tea/entryFiles-development/config$.js
@@ -2,6 +2,6 @@
 const g = typeof global !== 'undefined' ? global : self;
 g.appXAppJson = {
   "app": {
-    "$homepage": "pages/landing-page/landing-page"
+    "$homepage": "pages/card-selection/card-selection"
   }
 };
